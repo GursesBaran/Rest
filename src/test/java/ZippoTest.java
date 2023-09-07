85bdc2060cf37fbdb7d366ecf83652ea08905ff1
Baran Gurses <barangurses39@hotmail.com>
2023-09-07 15:49:02 -0400

    Rest

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -18,7 +18,9 @@ given()
         .when()
         .get("http://api.zippopotam.us/US/90210")
         .then()
-        .log().body();
+        .log().body()
+        .log().status()
+        .statusCode(200);
 
 
 
