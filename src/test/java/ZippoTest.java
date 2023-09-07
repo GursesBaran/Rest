c64ef018ead97a38a06c1bdf3b3d5e801384cab0
Baran Gurses <barangurses39@hotmail.com>
2023-09-07 15:45:28 -0400

    Rest

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,39 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ZippoTest {
+
+    @Test
+    void Test1() {
+
+        given()
+                .when()
+                .then();
+    }
+
+    @Test
+    void statusTest() {
+given()
+        .when()
+        .get("http://api.zippopotam.us/US/90210")
+        .then()
+        .log().body();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
