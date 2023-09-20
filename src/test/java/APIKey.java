8b18590a971c3d25bd079c439a5233f238d4211b
Baran Gurses <barangurses39@hotmail.com>
2023-09-20 18:56:49 -0400

    Rest

---
+++ src/test/java/APIKey.java
@@ -0,0 +1,83 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class APIKey {
+
+    @Test
+    void ApiKeyTest() {
+
+        given()
+                .header("x-api-key", "GwMco9Tpstd5vbzBzlzW9I7hr6E1D7w2zEIrhOra")
+                .when()
+                .get("https://l9njuzrhf3.execute-api.eu-west-1.amazonaws.com/prod/user")
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        //Use https://www.weatherapi.com/docs/ as a reference.
+        //First, You need to signup to weatherapi.com, and then you can find your API key under your account
+        //after that, you can use Java to request: http://api.weatherapi.com/v1/current.json?key=[YOUR-APIKEY]&q=Indianapolis&aqi=no
+        //Parse the json and print the current temperature in F and C.
+    }
+        @Test
+        void weatherAPI() {
+
+            Response response = given()
+                    .param("key", "b2b9681a1a3946e694a201643231507")
+                    .param("q", "Buffalo")
+                    .param("aqi", "no")
+                    .log().uri()
+                    .when()
+                    .get("http://api.weatherapi.com/v1/current.json")
+                    .then()
+                    .statusCode(200)
+                    //.log().body()
+                    .extract().response();
+
+            float temp_c = response.path("current.temp_c");
+            float temp_f = response.path("current.temp_f");
+
+            System.out.println("temp_c = " + temp_c);
+            System.out.println("temp_f = " + temp_f);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
